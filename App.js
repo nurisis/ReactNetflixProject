26a2693bafe6b64f86660b7ef5dac51224e66337
firesky11 <39053068+firesky11@users.noreply.github.com>
2019-10-10 20:59:52 +0900

    App.js 코드 정리

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 
 
 
-
+import React from 'react';
 import Navigator from './src/Navigator';
 
 
 
 
-export default Navigator;
 
-
-
+export default class App extends React.Component {
+  render() {
+    return (
+      <Navigator/>
+    );
+  }
+}
 
 
 
