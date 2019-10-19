2c7bdfd350a2525ccaf6575f39c4aee55dc60576
nurisis <isoyeong666@naver.com>
2019-10-19 10:59:39 +0900

    홈뷰 영상 플레이 없이

--- src/view/screen/main/Home.js
+++ src/view/screen/main/Home.js
@@ -12,7 +12,10 @@ class Home extends React.Component {
 
   
     state = {
-      data: randomUsers(20),  
+      dataA: randomUsers(20),  
+      dataB: randomUsers(20),  
+      dataC: randomUsers(20),  
+      dataD: randomUsers(20),  
       // video: { width: 300, height: 200, duration:"200" },
       // thumbnailUrl: "http://dkadf2kfhfqn2.cloudfront.net/images/video_thumbnail/hykwf678233_2019-10-04_13:29:23.jpg",
       videoUrl: "http://dkadf2kfhfqn2.cloudfront.net/videos/hykwf678233_2019-10-04_13:29:23.mp4",
@@ -24,7 +27,13 @@ class Home extends React.Component {
         'This is an action call!',
         [{ text: 'Aw yeah!' }]
       )}
-      
+      onFullScreen() {
+      Alert.alert(
+        'Boom',
+        'This is an action call!',
+        [{ text: 'Aw yeah!' }]
+      )}
+
     render() {
       
       const url = 'http://dkadf2kfhfqn2.cloudfront.net/videos/hykwf678233_2019-10-04_13:29:23.mp4'
@@ -38,7 +47,7 @@ class Home extends React.Component {
           <SafeAreaView style={styles.background}>
                 <Text style={styles.header}>Netflix 인기 콘텐츠</Text>
                 <FlatList
-                    data={this.state.data}
+                    data={this.state.dataA}
                     horizontal
                     style={styles.flatlist}
                     renderItem={({ item }) =>
@@ -51,7 +60,7 @@ class Home extends React.Component {
                 />
                 <Text style={styles.header}>지금 뜨는 콘텐츠</Text>
                 <FlatList
-                    data={this.state.data}
+                    data={this.state.dataB}
                     horizontal
                     style={styles.flatlist}
                     renderItem={({ item }) =>
@@ -65,7 +74,7 @@ class Home extends React.Component {
 
                 <Text style={styles.header}>SY 님이 시청 중인 콘텐츠</Text>
                 <FlatList
-                    data={this.state.data}
+                    data={this.state.dataC}
                     horizontal
                     style={styles.flatlist}
                     renderItem={({ item }) =>
@@ -79,7 +88,7 @@ class Home extends React.Component {
 
               <Text style={styles.header}>TV 프로그램/멀티캐스팅</Text>
                 <FlatList
-                    data={this.state.data}
+                    data={this.state.dataD}
                     horizontal
                     style={styles.flatlist}
                     renderItem={({ item }) =>
@@ -92,7 +101,10 @@ class Home extends React.Component {
                 />
 
                 <Text style={styles.header}>절찬 스트리밍 중</Text>
-                <View style={styles.container}>
+                {/* <View style={styles.container}>
+                  <Video url={url} />
+                </View> */}
+                {/* <View style={styles.container}>
                 <Video
                   autoPlay
                   url={url}
@@ -100,9 +112,10 @@ class Home extends React.Component {
                   logo={logo}
                   placeholder={placeholder}
                   onMorePress={() => this.onMorePress()}
-                  // onFullScreen={status => this.onFullScreen(status)}
+                  onFullScreen={() => this.onFullScreen()}
+                  onFullScreen={status => this.onFullScreen(status)}
                 />
-                </View>
+                </View> */}
                 {/* <VideoPlayer
                     endWithThumbnail
                     thumbnail={{ uri: this.state.thumbnailUrl }}
