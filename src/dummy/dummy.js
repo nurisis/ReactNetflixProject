c0580a1c89bb9768692eb21c5acc403fb02a592b
geontark <https://github.com/geontark>
2019-12-14 16:13:20 +0900

    더미 데이터 업데이트

--- src/dummy/dummy.js
+++ src/dummy/dummy.js
@@ -3,9 +3,14 @@
     {
       "id": "movieid",
       "name": "아이리시맨",
-      "thumNail": "http://img.movist.com/?img=/x00/05/22/96_p1.jpg",
+      "thumbNail": "http://img.movist.com/?img=/x00/05/22/96_p1.jpg",
       "comeoutDate": 20190401,
       "videoPath": "영화재생 경로",
+
+      "advertiseTitle" : "대부를 뛰어넘는 영화",
+      "summaryTitle" : "미국 갱스터 이야기",
+      "allowAge" : "19+",
+
       "summary": "전후 미국에 드리운 범죄 조직의 그림자. 이제 한 거물 암살자가 입을 연다. 마틴 스코세이지 감독과 로버트 드 니로, 알 파치노, 조 페시가 선사하는 장대한 범죄 드라마.",
       "actors": [
         "로버트니드로",
@@ -28,9 +33,13 @@
     {
       "id": "movieid",
       "name": "보헤미안 랩소디",
-      "thumNail": "http://img.movist.com/?img=/x00/05/05/99_p1.jpg",
+      "thumbNail": "http://img.movist.com/?img=/x00/05/05/99_p1.jpg",
       "comeoutDate": 20180801,
       "videoPath": "영화재생 경로",
+      "advertiseTitle" : "전세계를 뒤흔들었던 노래",
+      "summaryTitle" : "프레디 머큐리",
+      "allowAge" : "16+",
+
       "summary": "나는 스타가 되지 않을 것이다, 전설이 될 것이다. 퀸의 보컬 프레디 머큐리에 대한 이야기.",
       "actors": [
         "라미 말렉",
@@ -53,9 +62,15 @@
     {
       "id": "movieid",
       "name": "포드 V 페라리",
-      "thumNail": "https://pds.joins.com/news/component/htmlphoto_mmdata/201911/11/htm_2019111111542943324.jpg",
+      "thumbNail": "https://pds.joins.com/news/component/htmlphoto_mmdata/201911/11/htm_2019111111542943324.jpg",
       "comeoutDate": 20191217,
       "videoPath": "영화재생 경로",
+
+      "advertiseTitle" : "포드와 페라리를 원하는 자",
+      "summaryTitle" : "포드v 페라리",
+      "allowAge" : "15+",
+
+
       "summary": "1960년대, 매출 감소에 빠진 ‘포드’는 판매 활로를 찾기 위해  스포츠카 레이스를 장악한 절대적 1위 ‘페라리’와의 인수 합병을 추진한다. 막대한 자금력에도 불구, 계약에 실패하고 엔초 페라리로부터 모욕까지 당한 헨리 포드 2세는 르망 24시간 레이스에서 페라리를 박살 낼 차를 만들 것을 지시한다.",
       "actors": [
         "맷 데이먼",
@@ -78,9 +93,15 @@
     {
       "id": "movieid",
       "name": "어바웃타임",
-      "thumNail": "http://ticketimage.interpark.com/Movie/still_image/V13/V1302305p_01.gif",
+      "thumbNail": "http://ticketimage.interpark.com/Movie/still_image/V13/V1302305p_01.gif",
       "comeoutDate": 20130617,
       "videoPath": "영화재생 경로",
+
+      "advertiseTitle" : "시간여행자",
+      "summaryTitle" : "다시 과거로 돌아갈 수 있다면?",
+      "allowAge" : "전체",
+
+
       "summary": "원하는 시간으로 돌아가 인생을 변화시킨다. 그는 그토록 원하던 사랑을 이룰 수 있을까?",
       "actors": [
         "도널 글리슨",
@@ -102,9 +123,14 @@
     {
       "id": "movieid",
       "name": "모던패밀리",
-      "thumNail": "http://image.auction.co.kr/itemimage/16/c4/f2/16c4f2c3e6.jpg",
+      "thumbNail": "http://image.auction.co.kr/itemimage/16/c4/f2/16c4f2c3e6.jpg",
       "comeoutDate": 20111117,
       "videoPath": "영화재생 경로",
+
+      "advertiseTitle" : null,
+      "summaryTitle" : "다양한 가족의 삶을 그린 드라마",
+      "allowAge" : "전체",
+
       "summary": "저마다의 개성과 사연을 가진 세 가족의 따뜻하고 유쾌한 이야기",
       "actors": [
         "줄리보앤",
@@ -126,7 +152,7 @@
           "playList": [
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 1,
               "comeOutDate": 20190202,
@@ -135,7 +161,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 2,
               "comeOutDate": 20190302,
@@ -144,7 +170,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 3,
               "comeOutDate": 20190402,
@@ -153,7 +179,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 4,
               "comeOutDate": 20190402,
@@ -162,7 +188,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 5,
               "comeOutDate": 20190502,
@@ -171,7 +197,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 6,
               "comeOutDate": 20190602,
@@ -180,7 +206,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 7,
               "comeOutDate": 20190702,
@@ -189,7 +215,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 8,
               "comeOutDate": 20190202,
@@ -198,7 +224,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 9,
               "comeOutDate": 20190302,
@@ -207,7 +233,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 10,
               "comeOutDate": 20190402,
@@ -216,7 +242,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 11,
               "comeOutDate": 20190402,
@@ -225,7 +251,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 12,
               "comeOutDate": 20190502,
@@ -234,7 +260,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 13,
               "comeOutDate": 20190602,
@@ -243,7 +269,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 14,
               "comeOutDate": 20190702,
@@ -257,7 +283,7 @@
           "playList": [
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 1,
               "comeOutDate": 20190202,
@@ -266,7 +292,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 2,
               "comeOutDate": 20190302,
@@ -275,7 +301,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 3,
               "comeOutDate": 20190402,
@@ -284,7 +310,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 4,
               "comeOutDate": 20190402,
@@ -293,7 +319,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 5,
               "comeOutDate": 20190502,
@@ -302,7 +328,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 6,
               "comeOutDate": 20190602,
@@ -311,7 +337,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 7,
               "comeOutDate": 20190702,
@@ -320,7 +346,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 8,
               "comeOutDate": 20190202,
@@ -329,7 +355,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 9,
               "comeOutDate": 20190302,
@@ -338,7 +364,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 10,
               "comeOutDate": 20190402,
@@ -347,7 +373,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 11,
               "comeOutDate": 20190402,
@@ -356,7 +382,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 12,
               "comeOutDate": 20190502,
@@ -365,7 +391,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 13,
               "comeOutDate": 20190602,
@@ -374,7 +400,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 14,
               "comeOutDate": 20190702,
@@ -388,7 +414,7 @@
           "playList": [
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 1,
               "comeOutDate": 20190202,
@@ -397,7 +423,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 2,
               "comeOutDate": 20190302,
@@ -406,7 +432,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 3,
               "comeOutDate": 20190402,
@@ -415,7 +441,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 4,
               "comeOutDate": 20190402,
@@ -424,7 +450,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 5,
               "comeOutDate": 20190502,
@@ -433,7 +459,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 6,
               "comeOutDate": 20190602,
@@ -442,7 +468,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 7,
               "comeOutDate": 20190702,
@@ -451,7 +477,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 8,
               "comeOutDate": 20190202,
@@ -460,7 +486,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 9,
               "comeOutDate": 20190302,
@@ -469,7 +495,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 10,
               "comeOutDate": 20190402,
@@ -478,7 +504,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 11,
               "comeOutDate": 20190402,
@@ -487,7 +513,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 12,
               "comeOutDate": 20190502,
@@ -496,7 +522,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 13,
               "comeOutDate": 20190602,
@@ -505,7 +531,7 @@
             },
             {
               "id": "한회아이디",
-              "thumNail": "",
+              "thumbNail": "",
               "videoPath": "",
               "sequence": 14,
               "comeOutDate": 20190702,
@@ -521,6 +547,61 @@
         "isDibs": 0
       }
     }
+  ],
+
+
+  "SimilarVideos" : [
+    {
+      "id" : 'similarVideoId',
+      "thumbNail" : "https://i.ytimg.com/vi/PEAGc6qkbYI/maxresdefault.jpg"
+    },
+
+
+    {
+      "id" : 'similarVideoId',
+      "thumbNail" : "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000081/81992/81992_1000.jpg"
+    },
+    {
+      "id" : 'similarVideoId',
+      "thumbNail" : "http://2.bp.blogspot.com/_AQ8E1P1z3HA/Ruy6yLantsI/AAAAAAAAASo/UFxWfX66iXU/s400/Hannibal_Rising.jpg"
+    },
+    {
+      "id" : 'similarVideoId',
+      "thumbNail" : "http://images.sportskhan.net/article/2019/06/24/l_2019062402001044900215751.jpg"
+    },
+    {
+      "id" : 'similarVideoId',
+      "thumbNail" : "https://i.ytimg.com/vi/iY50AnMvuio/maxresdefault.jpg"
+    },
+    {
+      "id" : 'similarVideoId',
+      "thumbNail" : "https://i.ytimg.com/vi/gbbfl6aJ7EI/maxresdefault.jpg"
+    },
+    {
+      "id" : 'similarVideoId',
+      "thumbNail" : "https://t3.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/142z/image/EQmQDXQIjxR2DEkeAwYVKcUG0Ec.jpg"
+    },
+
+    {
+      "id" : 'similarVideoId',
+      "thumbNail" : "http://imgmovie.naver.com/mdi/mi/0366/C6651-05.jpg"
+    },
+
+
+    {
+      "id" : 'similarVideoId',
+      "thumbNail" : "http://image.auction.co.kr/itemimage/92/c6/5e/92c65e426.jpg"
+    },
+    {
+      "id" : 'similarVideoId',
+      "thumbNail" : "http://www.cinecafe.kr/files/attach/images/12486/035/056/5480dc8794a244781703283a4c66800d.jpg"
+    },
+    {
+      "id" : 'similarVideoId',
+      "thumbNail" : "https://pds.joins.com/news/component/moneytoday/201306/29/2013062814131227097_1.jpg"
+    },
+
+
   ]
 }
 
